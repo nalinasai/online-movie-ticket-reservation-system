dbf45eca0e28e86133ee70c671567ebe41d63084
nalinasai <nalinanalina995@gmail.com>
2025-03-26 23:33:05 +0530

    add further class needed

--- onlinemovie.java
+++ onlinemovie.java
@@ -10,6 +10,12 @@ class InvalidMovieCodeException extends Exception{
     }
 }
 
+class InvalidSeatsInput extends Exception{
+    public InvalidSeatsInput(String message){
+        super(message);
+    }
+}
+
 class Movie{
     String moviecode;
     String moviename;
@@ -34,6 +40,130 @@ class Movie{
     }
 }
 
+class bookings{
+    String customername;
+    
+    Movie selectedmovie;
+    String customerfile = "customer.csv";
+
+    bookings(String customername,Movie selectedmovie){
+        this.customername=customername;
+        
+        this.selectedmovie=selectedmovie;
+    }
+    
+    public int reservation(){
+        Scanner scan = new Scanner(System.in);
+        int seatsinput = 0;
+
+        while(true){
+            System.out.print("How many seats you want: ");
+            seatsinput = scan.nextInt();
+
+            try{
+                if(seatsinput<this.selectedmovie.availableseats){
+                    this.selectedmovie.availableseats =this.selectedmovie.availableseats - seatsinput;
+                    updateavailableseats(this.selectedmovie.availableseats);
+                    System.out.println("reserved sussess");
+                    break;
+                }
+                else{
+                    throw new InvalidSeatsInput("There is no available seat count you prefer. Enter again");
+                }
+            }
+            catch(InvalidSeatsInput e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return seatsinput;
+
+        
+    }
+
+    public void updateavailableseats(int updatedaseats){
+        String file = "Movie Reservation Dataset.csv";
+        List<String> lines = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+                String line;
+                br.readLine();
+                while((line=br.readLine())!=null){
+                    String[] details = line.split(",");
+                    if((this.selectedmovie.moviecode.equalsIgnoreCase(details[0])) && (this.selectedmovie.date.equalsIgnoreCase(details[2])) && (this.selectedmovie.showtime.equalsIgnoreCase(details[3]))){
+                        details[5] = String.valueOf(updatedaseats);
+                    }
+                    line = String.join(",",details);
+                    lines.add(line); 
+                }
+
+            
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+                bw.write("Movie Code,Movie Name,Date,Showtime,Total Seats,Available Seats,Ticket Price,Language,Genre");
+                bw.newLine();
+                for(String linewrite : lines){
+                    bw.write(linewrite);
+                    bw.newLine();
+                }
+        }
+        
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public double customerdetail(int seatsinput){
+        double totalprice = 0;
+        boolean fileexist = new File(customerfile).exists();
+        try(BufferedWriter bw1 = new BufferedWriter(new FileWriter(customerfile,true))){
+            if(!fileexist){
+                bw1.write("Customername,Movie name,show time,date,reserved seats, total price\n");
+            }
+            totalprice = seatsinput*this.selectedmovie.ticketprice;
+            bw1.write(this.customername+","+this.selectedmovie.moviename+","+this.selectedmovie.showtime+","+this.selectedmovie.date+","+seatsinput+","+totalprice+"\n");
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        return totalprice;
+    }
+
+
+    public void printthebill(double totalprice,int seatsinput){
+        String bill = "bill.txt";
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(bill))){
+            writer.write("============Online Movie Ticket\n\n");
+            writer.write("Customer name: "+customername+"\n");
+            writer.write("Movie name: "+this.selectedmovie.moviename+"\n");
+            writer.write("Date: "+this.selectedmovie.date+"\n");
+            writer.write("Show time: "+this.selectedmovie.showtime+"\n\n");
+            writer.write("-------------------------------------------------------\n");
+            writer.write("Booked seats count: "+seatsinput+"\n");
+            writer.write("Total price: "+totalprice+"\n");
+            writer.write("-------------------------------------------------------\n\n");
+            writer.write("bill saved in"+bill+"\n");
+            writer.write("THANKYOU");
+
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+
+
+    
+
+}
+
 public class onlinemovie{
     private static final String filename = "Movie Reservation Dataset.csv";
     private static Map<String, List<Movie>> itemsmap = new HashMap<>();
@@ -42,6 +172,10 @@ public class onlinemovie{
         loadthedetails();
 
         Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the customer name: ");
+        String customername = scan.nextLine();
+
         String code;
         List<Movie> Movies;
 
@@ -127,12 +261,11 @@ public class onlinemovie{
             }
         }
 
-        ///////
+        bookings book = new bookings(customername,selectedmovie);
+        int seatsinput = book.reservation();
+        double totalprice = book.customerdetail(seatsinput);
+        book.printthebill(totalprice,seatsinput);
 
-
-        
-        
-        
 
     }
 
@@ -166,5 +299,4 @@ public class onlinemovie{
         
 
     }
-
 }
